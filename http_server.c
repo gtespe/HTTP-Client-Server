4334af596378e633e2c0b49c05429b2a1044b25e
Grant Espe <gtespe@wpi.edu>
2016-09-09 19:15:52 -0400

    started basic http parsing for server, need to send file still

--- http_server.c
+++ http_server.c
@@ -7,8 +7,13 @@
 #include <string.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <time.h>
 
 #define MAXPENDING (5)
+#define IN_BUFFER_LEN (1025)
+#define OUT_BUFFER_LEN (1025)
+
+int handle_client(int socket);
 
 int main (int argc, char* argv[]){
 
@@ -66,13 +71,80 @@ int main (int argc, char* argv[]){
     return 0;
 }
 
-//Fetches the file and sends it to the client
+
+//Fetches the file and sends it to the client over the socket
+// Sends http statuses as well.
 int handle_client(int socket){
+    //Get the date for http
+    time_t t = time(NULL);
+    struct tm* date = localtime(&t);
 
-    char out_buffer[129];
-    sprintf(out_buffer, "HTTP/1.1 200 OK");
+    int bytesRcvd;
+    char in_buffer[IN_BUFFER_LEN]; 
 
+    if((bytesRcvd = recv(socket, &in_buffer, IN_BUFFER_LEN,0)) <= 0){
+        printf("\nrecv() failed or client disconnected, quitting...\n");
+        return 1;
+    }
     
+    char out_buffer[OUT_BUFFER_LEN];
+    char filename [125];
+    float http_version;
 
+    //replace the first \n with a null terminator
+    //   this is kind of a hacky way of making sure the sscanf only
+    //   reads the first line
+    for(int k = 0; k < bytesRcvd; k++){
+        if(in_buffer[k] == '\n'){
+            in_buffer[k] = '\0';
+            break;
+        }
+    }
+    
+    //check for sscanf errors
+    if(sscanf(in_buffer, "GET %s HTTP/%f\r", filename, http_version) != 2){
+        
+        //Request parsing error, send 403
+        printf("Invalid request, sending 403\n");
+        sprintf(out_buffer, "HTTP/1.1 403 Bad Request\r\n" 
+                            "Date: %c\r\n"
+                            "\r\n", date);
+        int out_len = strlen(out_buffer);
+        if(send(socket, out_buffer, out_len, 0) != out_len){
+            printf("\nsent different number of bytes than expected");
+        }
+        printf("403 sent\n");
+        return 1;
+    }
+    else{
+        //check if the file exists
+        if(access(filename, F_OK) != -1){
+            //File exists, send 200 ok, and then the file
+            sprintf(out_buffer, "HTTP/1.1 200 OK\r\n" 
+                                "Date: %c\r\n"
+                                "\r\n", date);
+
+            int out_len = strlen(out_buffer);
+            if(send(socket, out_buffer, out_len, 0) != out_len){
+                printf("\nsent different number of bytes than expected");
+            }
+            printf("200 sent\n");
+
+        }
+        else{
+            //file doesnt exist, send 404
+            sprintf(out_buffer, "HTTP/1.1 404 Not Found\r\n" 
+                                "Date: %c\r\n"
+                                "\r\n", date);
+
+            int out_len = strlen(out_buffer);
+            if(send(socket, out_buffer, out_len, 0) != out_len){
+                printf("\nsent different number of bytes than expected");
+            }
+            printf("404 sent\n");
+
+            return 1;
+        }
+    }
 }
 
